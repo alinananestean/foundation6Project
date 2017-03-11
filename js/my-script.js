826a56867d39d4857b0638727c0ae90460ac037d
Alina <nanesteanalina@gmail.com>
2017-03-11 10:43:17 +0200

    Initial commit

---
+++ js/my-script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alina on 3/3/2017.
+ */
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+        $('.nav-menu').toggleClass('hidden-sm-down');
+    });
+});
