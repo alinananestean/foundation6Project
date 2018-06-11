a43d939e9112b56eb53d4069988898c3a4f363ed
Alina <nanesteanalina@gmail.com>
2018-06-11 12:27:35 +0300

    Add some text.

--- js/my-script.js
+++ js/my-script.js
@@ -2,8 +2,11 @@
  * Created by Alina on 3/3/2017.
  */
 $(document).ready(function(){
-    $('#nav-icon').click(function(){
-        $(this).toggleClass('open');
-        $('.nav-menu').toggleClass('hidden-sm-down');
-    });
+    // $('#nav-icon').click(function(){
+    //     $(this).toggleClass('open');
+    //     $('.nav-menu').toggleClass('hidden-sm-down');
+    // });
+
+
+
 });
